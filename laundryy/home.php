d37245b312e89978e0d59b80dcf055d8c6f9321e
Fatimah Azzahra <fatimahazzahra4121@gmail.com>
2022-10-24 08:59:52 +0700

    Add files via upload

---
+++ laundryy/home.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php";
+?>
+<div class="page-banner home-banner">
+  <div class="container h-100">
+    <div class="row align-items-center h-100">
+      <div class="col-lg-6 py-3 wow fadeInUp">
+        <h1><strong>Laun<span class="text-primary">Dry.</span></strong></h1>
+        <p class="text-lg mb-5">Web Laundry yang dibuat oleh Fatimah Azzahra Jannatul Mursid
+          XI RPL 4 Absen 11. Bismillah semoga lulus UKL.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<html>
